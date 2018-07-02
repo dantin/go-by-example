331ece4b3c1984aa05938ad1a66b6dc8ffbfb504
dantin <chengjie.ding@gmail.com>
2018-07-02 10:37:45 +0800

    add 'welcome' module in 'A Tour of Go'.

---
+++ tour/welcome/hello.go
@@ -0,0 +1,8 @@
+// version 1.0 2018-07-02
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
---
+++ tour/welcome/sandbox.go
@@ -0,0 +1,13 @@
+// version 1.0 2018-07-02
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
