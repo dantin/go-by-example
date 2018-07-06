c7d994e947f4654e1329fedac86f3d4f153d46c8
dantin <chengjie.ding@gmail.com>
2018-07-06 13:52:36 +0800

    finish demos.

---
+++ examples/command-line-arguments.go
@@ -0,0 +1,18 @@
+// version 1.0 2018-07-06
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	arg := os.Args[3]
+
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+}
