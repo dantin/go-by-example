9630a133efee4e824d3f3d6062b2ca2edc9f976c
dantin <chengjie.ding@gmail.com>
2018-07-09 15:07:03 +0800

    sample of array.

---
+++ effective-go/arrays.go
@@ -0,0 +1,17 @@
+// version 1.0 2018-07-09
+package main
+
+import "fmt"
+
+func Sum(a *[3]float64) (sum float64) {
+	for _, v := range *a {
+		sum += v
+	}
+	return
+}
+
+func main() {
+	array := [...]float64{7.0, 8.5, 9.1}
+	x := Sum(&array)
+	fmt.Println(x)
+}
