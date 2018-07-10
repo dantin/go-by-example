fa852dd25721f6ceafe6408a4d866dd67b78ec4b
dantin <chengjie.ding@gmail.com>
2018-07-10 16:03:09 +0800

    demo code before '1.3'.

---
+++ gopl/ch1/ex1.4.go
@@ -0,0 +1,38 @@
+// version 1.0 2018-07-10
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		if hasDuplicatedLines(f) {
+			fmt.Println(arg)
+		}
+		f.Close()
+	}
+}
+
+func hasDuplicatedLines(f *os.File) bool {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		if _, ok := counts[line]; ok {
+			return true
+		} else {
+			counts[line]++
+		}
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	return false
+}
