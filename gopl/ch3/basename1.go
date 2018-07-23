7ee5433e31c8f38ffc8f96faa04f5a47db199417
dantin <chengjie.ding@gmail.com>
2018-07-23 16:20:47 +0800

    sample code of 'basename1'.

---
+++ gopl/ch3/basename1.go
@@ -0,0 +1,32 @@
+// version 1.0 2018-07-23
+
+// basename removes directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+package main
+
+import "fmt"
+
+func basename(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// Preserve everything before last '.'.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main() {
+	sa := []string{"a", "a.go", "a/b/c.go", "a/b.c.go"}
+	for _, s := range sa {
+		fmt.Printf("%s => %s\n", s, basename(s))
+	}
+}
