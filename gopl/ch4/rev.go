f4fe07fe8b365066f7cd201a5aae4925434afb58
dantin <chengjie.ding@gmail.com>
2018-07-25 13:57:39 +0800

    dome code for slice comparation.

---
+++ gopl/ch4/rev.go
@@ -0,0 +1,46 @@
+// version 1.0 2018-07-25
+
+package main
+
+import "fmt"
+
+// reverse reverses a slice of ints in place.
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// shfl rotate a slice left by n positions.
+func shfl(s []int, n int) {
+	// Rotate s left by n positions.
+	reverse(s[:n])
+	reverse(s[n:])
+	reverse(s)
+}
+
+// equal compare whether two string slices are equal.
+// NOTE: There is no language support to compare whether two
+//       slice are equal.
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(a)
+	// convert array to slice.
+	s := a[:]
+	shfl(s, 2)
+	fmt.Println(a)
+	reverse(a[:])
+	fmt.Println(a)
+}
