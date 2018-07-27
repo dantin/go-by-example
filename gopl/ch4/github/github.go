29ea9549c79a5c2ccfcf9b71b2690feb75f60fe8
dantin <chengjie.ding@gmail.com>
2018-07-27 15:05:48 +0800

    solution of 'ex4.11'.

--- gopl/ch4/github/github.go
+++ gopl/ch4/github/github.go
@@ -5,15 +5,20 @@
 package github
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 )
 
-const IssueURL = "https://api.github.com/search/issues"
+const (
+	IssueURL = "https://api.github.com/search/issues"
+	APIURL   = "https://api.github.com"
+)
 
 type IssueSearchResult struct {
 	TotalCount int `json:"total_count"`
@@ -28,6 +33,10 @@ type Issue struct {
 	User      *User
 	CreatedAt time.Time `json:"created_at"`
 	Body      string    // in Markdown format
+}
+
+func (i Issue) CacheURL() string {
+	return fmt.Sprintf("/issues/%d", i.Number)
 }
 
 type User struct {
@@ -56,4 +65,61 @@ func SearchIssues(terms []string) (*IssueSearchResult, error) {
 	}
 	resp.Body.Close()
 	return &result, nil
+}
+
+func get(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("can't get %s: %s", url, resp.Status)
+	}
+	return resp, nil
+}
+
+func GetIssue(owner, repo, number string) (*Issue, error) {
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues", number}, "/")
+	resp, err := get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var issue Issue
+	if err := json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	return &issue, nil
+}
+
+func EditIssue(owner, repo, number string, fields map[string]string) (*Issue, error) {
+	buf := &bytes.Buffer{}
+	encoder := json.NewEncoder(buf)
+	err := encoder.Encode(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues", number}, "/")
+	req, err := http.NewRequest("PATCH", url, buf)
+	req.SetBasicAuth(os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_PASS"))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to edit issue: %s", resp.Status)
+	}
+	var issue Issue
+	if err = json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	return &issue, nil
 }
