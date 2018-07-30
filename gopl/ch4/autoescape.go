7cf6c778762c42a53fb06cb1c869c62b51c1ec26
David Ding <chengjie.ding@gmail.com>
2018-07-30 13:13:00 +0800

    finish chapter4 of gopl. (#14)
    * demo code for 'text/template'.
    
    * demo of 'html/template'.
    
    * demo of escape using 'template.HTML' instead of string.
    
    * solution of 'ex4.14'.

---
+++ gopl/ch4/autoescape.go
@@ -0,0 +1,23 @@
+// version 1.0 2018-07-30
+
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	const templ = `<p>A: {{.A}}</p><p>B: {{.B}}</p>`
+	t := template.Must(template.New("escape").Parse(templ))
+	var data struct {
+		A string        // untrusted plain text
+		B template.HTML // trusted HTML
+	}
+	data.A = "<b>Hello</b>"
+	data.B = "<b>Hello</b>"
+	if err := t.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+}
