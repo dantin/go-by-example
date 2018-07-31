47144d4341a4e1c47ac237882c0dbe95f19034ea
David Ding <chengjie.ding@gmail.com>
2018-07-31 17:20:47 +0800

    demo & exec of '5.5 (#16)
    * demo of return multiple values to caller.
    
    * solution of 'ex5.5'.
    
    * solution of 'ex5.6'.
    
    * demo that shows how to retry failed operation, possibly with a delay
    between tries.
    
    * demo of function as parameter.
    
    * solution of 'ex5.7'.
    
    * solution of 'ex5.8'.
    
    * solution of 'ex5.9'.

---
+++ gopl/ch5/wait.go
@@ -0,0 +1,27 @@
+// version 1.0 2018-07-31
+
+// WaitForServer attempts to conact the server of a URL.
+// It tries for one minute using exponential back-off.
+// It reports an error if all attemps fail.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func WaitForServer(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
+		if err == nil {
+			return nil // success
+		}
+		log.Printf("server not responding (%s); retrying...", err)
+		time.Sleep(time.Second << uint(tries)) // exponential back-off
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
+}
