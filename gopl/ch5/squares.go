a02f95a3b8ca9538273455c1829aea22e231ce07
dantin <chengjie.ding@gmail.com>
2018-08-02 18:06:37 +0800

    demo of a closure.

---
+++ gopl/ch5/squares.go
@@ -0,0 +1,23 @@
+// version 2018-08-02
+
+// squares returns a function that returns
+// the next square number each time it is called.
+package main
+
+import "fmt"
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
