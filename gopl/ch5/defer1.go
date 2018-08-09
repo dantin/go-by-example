90b092c81593783c0da6f9c386d9f7fc55498e07
dantin <chengjie.ding@gmail.com>
2018-08-09 14:05:43 +0800

    panic that terminate program.

---
+++ gopl/ch5/defer1.go
@@ -0,0 +1,14 @@
+// version 1.0 2018-08-09
+package main
+
+import "fmt"
+
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
+}
+
+func main() {
+	f(3)
+}
