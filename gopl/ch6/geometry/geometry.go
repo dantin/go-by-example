1aeee62433403b2a9244f443eb61f3a7017a2c59
dantin <chengjie.ding@gmail.com>
2018-08-10 17:53:06 +0800

    demo to show method expression.

--- gopl/ch6/geometry/geometry.go
+++ gopl/ch6/geometry/geometry.go
@@ -12,7 +12,7 @@ type Point struct{ X, Y float64 }
 //}
 
 // Distance method
-func (p *Point) Distance(q Point) float64 {
+func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
