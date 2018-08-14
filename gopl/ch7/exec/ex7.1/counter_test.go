0242d2c1204d23271935a466430343cac6469e41
dantin <chengjie.ding@gmail.com>
2018-08-14 11:33:43 +0800

    solution of 'ex7.1'.

---
+++ gopl/ch7/exec/ex7.1/counter_test.go
@@ -0,0 +1,43 @@
+// version 1.0 2018-08-14
+package counter
+
+import "testing"
+
+func TestLineCounter(t *testing.T) {
+	c := &LineCounter{}
+	p := []byte("one\ntwo\nthree\n")
+	n, err := c.Write(p)
+	if n != len(p) {
+		t.Logf("len: %d != %d", n, len(p))
+		t.Fail()
+	}
+	if err != nil {
+		t.Log("err: ", err)
+		t.Fail()
+	}
+	if c.N() != 3 {
+		t.Logf("line: %d != %d", c.N(), 3)
+		t.Fail()
+	}
+}
+
+func TestWordCounter(t *testing.T) {
+	c := &WordCounter{}
+	data := [][]byte{
+		[]byte("The upcoming word is sp"),
+		[]byte("lit across the buffer boundary."),
+		[]byte(" And this one ends on the buffer boundary."),
+		[]byte(" Last words."),
+	}
+	for _, p := range data {
+		n, err := c.Write(p)
+		if n != len(p) || err != nil {
+			t.Logf(`bad write: p="%s" n=%d err="%s"`, string(p), n, err)
+			t.Fail()
+		}
+	}
+	if c.N() != 19 {
+		t.Logf("words: %d != %d", c.N(), 19)
+		t.Fail()
+	}
+}
