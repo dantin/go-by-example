54114f84fc971bb0fada2e01ce97f89f43f6dc80
dantin <chengjie.ding@gmail.com>
2018-08-16 11:10:01 +0800

    solution of 'ex7.3'.

---
+++ gopl/ch7/exec/ex7.3/sort_test.go
@@ -0,0 +1,14 @@
+// version 1.0 2018-08-16
+package treesort
+
+import "testing"
+
+func TestString(t *testing.T) {
+	root := &tree{value: 3}
+	root = add(root, 2)
+	root = add(root, 4)
+	if root.String() != "[2 3 4]" {
+		t.Log(root)
+		t.Fail()
+	}
+}
