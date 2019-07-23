28f80964b2228bdd53f432ad78ec428b21854a60
David Ding <chengjie.ding@gmail.com>
2019-07-23 13:19:38 +0800

    fix test bug. (#33)

--- gopl/ch9/memo1/memo_test.go
+++ gopl/ch9/memo1/memo_test.go
@@ -1,19 +1,20 @@
-package memo
+package memo_test
 
 import (
 	"testing"
 
+	memo "github.com/dantin/go-by-example/gopl/ch9/memo1"
 	"github.com/dantin/go-by-example/gopl/ch9/memotest"
 )
 
 var httpGetBody = memotest.HTTPGetBody
 
 func TestSequential(t *testing.T) {
-	m := New(httpGetBody)
+	m := memo.New(httpGetBody)
 	memotest.Sequential(t, m)
 }
 
 func TestConcurrent(t *testing.T) {
-	m := New(httpGetBody)
+	m := memo.New(httpGetBody)
 	memotest.Concurrent(t, m)
 }
