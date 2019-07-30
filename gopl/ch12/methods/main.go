e5ad1b72475c27900e908f0f0d18d33216b6da43
dantin <chengjie.ding@gmail.com>
2019-07-30 14:49:45 +0800

    add sample code of chapter 12.

---
+++ gopl/ch12/methods/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// Print prints the method set of the value x.
+func Print(x interface{}) {
+	v := reflect.ValueOf(x)
+	t := v.Type()
+	fmt.Printf("type %s\n", t)
+
+	for i := 0; i < v.NumMethod(); i++ {
+		methType := v.Method(i).Type()
+		fmt.Printf("func (%s) %s%s\n", t, t.Method(i).Name,
+			strings.TrimPrefix(methType.String(), "func"))
+	}
+}
+
+func main() {
+	Print(time.Hour)
+
+	fmt.Println()
+
+	Print(new(strings.Replacer))
+}
---
+++ gopl/ch12/search/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dantin/go-by-example/gopl/ch12/params"
+)
+
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels    []string `http:"l"`
+		MaxResult int      `http:"max"`
+		Exact     bool     `http:"x"`
+	}
+	data.MaxResult = 10 // set default
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest) // 400
+		return
+	}
+
+	// ...rest of handler...
+	fmt.Fprintf(resp, "Search: %+v\n", data)
+}
+
+func main() {
+	http.HandleFunc("/search", search)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
